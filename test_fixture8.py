01c0d7e47454b916f9965de6a4c586fdd2febda8
Lion-78 <a.untilov@gmail.com>
2021-10-01 18:18:50 +0300

    Check markers for classes and for tests

--- test_fixture8.py
+++ test_fixture8.py
@@ -12,8 +12,22 @@ def browser():
     print("\nquit browser..")
     browser.quit()
 
-
+@pytest.mark.smoke
 class TestMainPage1():
+
+    @pytest.mark.smoke
+    def test_guest_should_see_login_link(self, browser):
+        browser.get(link)
+        browser.find_element_by_css_selector("#login_link")
+
+    @pytest.mark.regression
+    def test_guest_should_see_basket_link_on_the_main_page(self, browser):
+        browser.get(link)
+        browser.find_element_by_css_selector(".basket-mini .btn-group > a")
+
+
+@pytest.mark.regression
+class TestMainPage2():
 
     @pytest.mark.smoke
     def test_guest_should_see_login_link(self, browser):

364b9386e17cb635e2d6ddaaee09e98775a58647
Lion-78 <a.untilov@gmail.com>
2021-10-01 23:16:06 +0300

    Initial commit

--- test_fixture7.py
+++ test_fixture7.py
@@ -9,8 +9,32 @@ def browser():
     print("\nquit browser..")
     browser.quit()
 
-@pytest.mark.parametrize('language', ["ru", "en-gb"])
-def test_guest_should_see_login_link(browser, language):
-    link = f"http://selenium1py.pythonanywhere.com/{language}/"
+
+
+languages = [
+    ("ru", "русский"),
+    ("de", "немецкий"),
+    pytest.param("ua", "украинский",  marks=pytest.mark.xfail(reason="no ua language yet")),
+    ("en-gb", "английский")
+]
+
+
+@pytest.mark.parametrize('code, language', languages)
+def test_guest_should_see_login_link(browser, code, language):
+    link = "http://selenium1py.pythonanywhere.com/{}/".format(code)
+    print("Проверяемый язык %s" % language)
     browser.get(link)
     browser.find_element_by_css_selector("#login_link")
+
+
+@pytest.mark.parametrize('language', ["ru", "en-gb"])
+class TestLogin:
+    def test_guest_should_see_login_link(self, browser, language):
+        link = f"http://selenium1py.pythonanywhere.com/{language}/"
+        browser.get(link)
+        browser.find_element_by_css_selector("#login_link")
+        # этот тест запустится 2 раза
+
+    def test_guest_should_see_navbar_element(self, browser, language):
+        # этот тест тоже запустится дважды
+        pass
